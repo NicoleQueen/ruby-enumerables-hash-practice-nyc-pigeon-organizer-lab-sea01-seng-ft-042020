fba71c61fe10581c7741e708e4d23ff94a38e73a
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 07:43:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  temp1 = [] # 
+  temp2 = []
+  data.values.each do |element|
+    temp1 = element.values
+  end
+  i = 0 
+  j = 0 
+  while i < temp1.length 
+    while j < temp1[i] 
+      !temp2.include? temp1[i][j]
+      temp2<<temp1[i][j]
+    end
+  end
+  
+  
+  pigeon_list
 end
