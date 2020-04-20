0b1c3fdc31f05985a0f52fdefd945dd7eaf470f2
QIuyan Peng <nicolepeng99@gmail.com>
2020-04-20 07:43:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,8 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
-  temp1 = [] # 
-  temp2 = []
+  temp1 = [] # temporary values with redandancy
+  temp2 = [] # keys to pigeon_list
   data.values.each do |element|
     temp1 = element.values
   end
@@ -11,10 +11,29 @@ def nyc_pigeon_organizer(data)
   while i < temp1.length 
     while j < temp1[i] 
       !temp2.include? temp1[i][j]
-      temp2<<temp1[i][j]
+      temp2 << temp1[i][j]
     end
   end
   
+  pigeon_list_keys = temp2 # keys to pigeon_list
+  data_keys = data.keys # keys to keys to pigeon_list
   
+  pigeon_list_keys.each do |plk| # e.g., "Lucky"
+    plk_hash = {} # {"color": ["purple"], "gender": ["male"], "lives": [Central Park"]}
+    data_keys.each do |dk| # e.g,. "color"
+      dk_value = []
+      # find dk_value
+      original_dk_hash = data[dk]
+      # (1) iterate over original_dk_hash's key
+      # (2) check if the value contains plk, if yes add the key to dk_value
+      original_dk_hash.each do |k, v| 
+        v.include? plk
+        dk_value << v
+      end
+      
+      plk_hash[dk] = dk_value
+    end
+    pigeon_list[plk] = plk_hash
+  end  
   pigeon_list
 end
